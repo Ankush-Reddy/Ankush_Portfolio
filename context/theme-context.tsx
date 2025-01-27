896570bf8c712b8618d9bd88524300917d7f88ad
ankushreddy <ralucky@iu.edu>
2025-01-27 18:13:13 -0500

    Comitted

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -63,7 +63,7 @@ export function useTheme() {
   const context = useContext(ThemeContext);
 
   if (context === null) {
-    throw new Error("useTheme must be used within a ThemeContextProvider");
+    throw new Error("useTheme must be used with in a ThemeContextProvider");
   }
 
   return context;
